3ae60772b9dd91f36780500bfb4941b531599a77
andbudha <andreibartov@gmail.com>
2023-09-19 13:52:24 +0200

    marker for company added

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -24,6 +24,12 @@ export class CustomMap {
     })
   }
   addCompanyMarker(company: Company) {
-
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng
+      }
+    })
   }
 }
