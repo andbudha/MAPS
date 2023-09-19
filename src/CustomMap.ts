e887e58e1a4e028389b3e352da8928c4d59c75db
andbudha <andreibartov@gmail.com>
2023-09-19 14:29:03 +0200

    showing popup on marker-click

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,3 @@
-import { Company } from "./Company";
-import { User } from "./User";
-
 
 interface Mappable {
   location: {
@@ -23,12 +20,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable) {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
-    })
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+      infoWindow.open(this.googleMap, marker)
+    });
   }
 }
