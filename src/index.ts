e808d66ea5625b835da1d618347b6d9100633d90
andbudha <andreibartov@gmail.com>
2023-09-19 10:58:00 +0200

    CustomMap class created

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 /// <reference types="@types/google.maps" />
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
 const user = new User();
 const company = new Company();
 
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement,
-  {
-    zoom: 1,
-    center: {
-      lat: 0,
-      lng: 0
-    }
-  });
+new CustomMap('map');
